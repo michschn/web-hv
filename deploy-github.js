1d378005e1d5689718843b9ad79e1633ac43e1e6
Mike Schneider <michschn@google.com>
2022-10-03 15:07:35 +0200

    Replace `gh-pages` with `angular-cli-ghpages`.
    using `npm run deploy` now correctly updates `<base href="">`

--- deploy-github.js
+++
@@ -1,9 +0,0 @@
-var ghpages = require('gh-pages');
-
-ghpages.publish('dist', (err) => {
-    if (err) {
-        console.error('Deploy failed!', err)
-    } else {
-        console.log('Deploy Complete!')
-    }
-});
